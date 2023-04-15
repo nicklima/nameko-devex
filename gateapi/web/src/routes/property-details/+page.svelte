5276dadda7ce5b24ec0c45190ac008184afc143f
Nick Lima <nick.lima.wp@gmail.com>
2023-04-15 19:01:17 -0300

    wip: cleaning code / fix date issue

--- gateapi/web/src/routes/property-details/+page.svelte
+++ gateapi/web/src/routes/property-details/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import {
+		Breadcrumb,
 		InputChoice,
 		InputDate,
 		InputMoney,
@@ -10,32 +11,25 @@
 	import { Baths, Beds, Condo, House, Profile } from '@icons'
 
 	export const typeOptions = [
-		{
-			type: 'Single-Family',
-			icon: House,
-		},
-		{
-			type: 'Multi-Family',
-			icon: Condo,
-		},
-		{
-			type: 'Townhome',
-			icon: House,
-		},
-		{
-			type: 'Condo',
-			icon: Condo,
-		},
+		{ type: 'Single-Family', icon: House },
+		{ type: 'Multi-Family', icon: Condo },
+		{ type: 'Townhome', icon: House },
+		{ type: 'Condo', icon: Condo },
 	]
 
 	let formData = {} as FormData
-	$: console.log(formData)
+	const handleSubmit = () => console.log(formData)
 </script>
 
+<svelte:head>
+	<title>Nameko | Property details</title>
+</svelte:head>
+
 <div class="flex mx-auto flex-col align-center max-w-4xl p-5">
-	<form action="" class="grid grid-cols-12 gap-5 pt-20">
+	<Breadcrumb pageName="Property details" />
+	<form class="grid grid-cols-12 gap-5 pt-16" on:submit={handleSubmit}>
 		<div class="col-span-12 md:col-span-6">
-			<h3 class="text-3xl">Property details</h3>
+			<h1 class="text-3xl">Property details</h1>
 			<p class="mt-5">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -124,12 +118,12 @@
 					maxlength={6}
 					bind:value={formData.sqFt}
 				/>
-				<InputDate
+				<!-- <InputDate
 					label="Available On"
 					name="availableOn"
 					placeholder="Select a date"
 					bind:value={formData.availableOn}
-				/>
+				/> -->
 				<InputSelect
 					label="Vacancy"
 					icon={Profile}
@@ -139,16 +133,17 @@
 				/>
 			</div>
 		</div>
+		<div class="flex w-full mt-8 justify-between col-span-12">
+			<a
+				href="/"
+				class="py-3 px-8 rounded-full border bg-transparent border-slate-500 transition-all text-slate-500 uppercase hover:text-slate-900 hover:border-slate-900 font-thin"
+				>Back</a
+			>
+			<button
+				type="submit"
+				class="py-3 px-8 rounded-full bg-violet-700 text-white uppercase transition-all hover:bg-violet-900 font-thin"
+				>Continue</button
+			>
+		</div>
 	</form>
-
-	<div class="flex w-full mt-8 justify-between">
-		<button
-			class="py-3 px-8 rounded-full border bg-transparent border-slate-500 transition-all text-slate-500 uppercase hover:text-slate-900 hover:border-slate-900 font-thin"
-			>Back</button
-		>
-		<button
-			class="py-3 px-8 rounded-full bg-violet-700 text-white uppercase transition-all hover:bg-violet-900 font-thin"
-			>Continue</button
-		>
-	</div>
 </div>
