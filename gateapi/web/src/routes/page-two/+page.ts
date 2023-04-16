8695ab16e1b549e6012a8c0722c31413a983a539
Nick Lima <nick.lima.wp@gmail.com>
2023-04-16 20:53:04 -0300

    starting the api tests

--- gateapi/web/src/routes/page-two/+page.ts
+++ gateapi/web/src/routes/page-two/+page.ts
@@ -7,29 +7,7 @@ export async function load({ fetch }) {
 		return product
 	}
 
-	const mockedProduct: ProductResponse = {
-		id: 1,
-		order_details: [
-			{
-				id: 1,
-				quantity: 1,
-				product_id: 'the_odyssey',
-				image: 'https://images.unsplash.com/photo-1623020874699-0ddd626114a8',
-				// image: 'http://www.example.com/airship/images/the_odyssey.jpg',
-				price: '100000.99',
-				product: {
-					maximum_speed: 5,
-					id: 'the_odyssey',
-					title: 'The Odyssey',
-					passenger_capacity: 101,
-					in_stock: 9,
-				},
-			},
-		],
-	}
-
 	return {
-		product: mockedProduct,
-		//fetchResponse()
+		product: fetchResponse(),
 	}
 }
