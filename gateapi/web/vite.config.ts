c647e17a238851c683b7d30ae807a42f76f76f20
Nick Lima <nick.lima.wp@gmail.com>
2023-04-17 12:29:27 -0300

    adding cross-env and ENDPOINT rule in vite config

--- gateapi/web/vite.config.ts
+++ gateapi/web/vite.config.ts
@@ -1,12 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 
+const ENDPOINT =
+	process.env.NODE_ENV === 'development'
+		? 'http://127.0.0.1:9000/'
+		: 'http://127.0.0.1:4173'
+
 export default {
 	plugins: [sveltekit()],
 	server: {
 		port: 9000,
 		proxy: {
 			'/orders': {
-				target: 'http://127.0.0.1:8000/',
+				target: ENDPOINT,
 				changeOrigin: true,
 				secure: false,
 			},
